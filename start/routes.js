01da062960ef74fadd9aa84f9a7d6123cdb28109
Luann Pereira Mendes <luannpmendes@gmail.com>
2025-02-19 08:28:20 -0300

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -1,14 +1,26 @@
 const Route = use('Route');
 
 // Root route
-Route.get('/', async ({ response }) => {
-  return response.send({
-    message: 'Servidor AdonisJS está rodando!',
-    routes: [
-      { method: 'POST', path: '/calculate-average', description: 'Calcula a média de um array de 10 números.' },
-    ],
-  });
-});
+Route.get('/', async ({ response, request }) => {
+  const numbers = request.input('numbers') // Pega o array de números enviado na query string
+
+  // Verifica se o array tem exatamente 10 números
+  if (numbers && numbers.length === 10) {
+    // Calcula a média
+    const total = numbers.reduce((acc, num) => acc + num, 0)
+    const average = total / numbers.length
+
+    return response.send({
+      message: 'Servidor AdonisJS está rodando!',
+      average: average
+    })
+  } else {
+    return response.status(400).send({
+      message: 'Envie exatamente 10 números na query string.'
+    })
+  }
+})
+
 
 // Using the controller with JWT middleware
 Route.post('/calculate-average', 'CalculateAverage.metodo').middleware(['auth']);
